2887cab8b8a41d277be9a2a823ca7855f483935f
JoseMGuerra <jose_mgr@hotmail.com>
2023-02-19 18:06:13 +0000

    Add product_detail view and template

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
 
 
@@ -15,6 +15,25 @@ def product_list(request):
     template = ["products/list.html"]
     context = {
         'products': products,
+    }
+
+    return render(request, template, context)
+
+
+def product_detail(request, product_id):
+    """
+    Display an individual product details
+    Parameters:
+        request (HttpRequest): the HTTP request made to the view.
+        product_id: The unique identifier for the product.
+    Returns:
+        HttpResponse: The rendered HTML template for the product detail page.
+    """
+    product = get_object_or_404(Product, pk=product_id)
+
+    template = ["products/detail.html"]
+    context = {
+        'product': product,
     }
 
     return render(request, template, context)
