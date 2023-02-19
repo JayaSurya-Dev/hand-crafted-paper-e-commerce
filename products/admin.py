1b099128e12dfdc9fe9704eedb98d2f3178fce56
JoseMGuerra <jose_mgr@hotmail.com>
2023-02-19 12:36:44 +0000

    Add product_list view and template

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,41 @@
 from django.contrib import admin
 from .models import Category, Product
+from django.contrib.auth.decorators import login_required
+
+admin.site.login = login_required(admin.site.login)
 
 
-admin.site.register(Product)
-admin.site.register(Category)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+        'slug',
+        )
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'slug',
+        'category',
+        'price',
+        'rating',
+        'available',
+        'created_on',
+        'updated_on',
+        )
+
+    list_filter = (
+        'available',
+        'created_on',
+        'updated_on',
+    )
+
+    list_editable = (
+        'price',
+        'available',
+    )
+    prepopulated_fields = {'slug': ('name',)}
--- products/views.py
+++ products/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from .models import Category, Product
 
-# Create your views here.
+
+def product_list(request):
+    """
+    Display a list of all products, including sorting and search queries
+    Parameters:
+        request (HttpRequest): the HTTP request object
+    Returns:
+        HttpResponse: the HTTP response object with the rendered template
+    """
+    products = Product.objects.all()
+
+    template = ["products/list.html"]
+    context = {
+        'products': products,
+    }
+
+    return render(request, template, context)
